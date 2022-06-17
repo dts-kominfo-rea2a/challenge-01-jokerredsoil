ef9cbf269485b7cde0beb5aac65f1a517de98340
jokerredsoil <septianxnugraha@gmail.com>
2022-06-17 10:22:35 +0700

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,61 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+const arrColorMonica = ["Yellow", "Pink", "White", "Purple"];
+
+const arrRestoMonica = ["Bento", "Sushi", "Pancake", "Eggy", "Tempura", "Bento", "Eggy", "Padang", "Tteok", "Sushi", "Sushi"]; 
+
+const educationMonica = [
+    { name: "SD 01", city: "Jakarta", graduate: "2016" },
+    { name: "SMP 02", city: "Jakarta", graduate: "2019" },
+    { name: "SMA 01", city: "Surabaya", graduate: "2016"},
+    {name: "Universitas Maju", city: "Tangerang", graduate: ""}
+    
+]
+
+let setColorMonica = new Set(arrColorMonica);
+let setRestoMonica = new Set(arrRestoMonica);
+
+const arrColorWendy = ["Blue", "Black", "Grey"];
+const arrRestoWendy = ["Tempura", "Bento", "Sushi", "Pancake", "Padang", "Katsu", "Geprek", "Pancake", "Eggy"]; 
+
+const educationWendy = [
+    {name: "SD 02", city: "Jakarta", graduate: "2010"},
+    {name: "SMP 03", city: "Bogor", graduate: "2013"},
+    {name: "SMA 01", city: "Surabaya", graduate: "2016"},
+    {name: "Universitas Maju", city: "Tangerang",graduate:""}
+
+]
+
+let setColorWendy = new Set(arrColorWendy);
+let setRestoWendy = new Set(arrRestoWendy);
+
+
+const firstUser = {
+name: "Monica",
+gender: "Female",
+age: 17,
+email: "monica@dingdong.com",
+favoriteColor: [...setColorMonica],
+isHavePet: "Yes",
+education: educationMonica,
+favoriteRestaurant: [...setRestoMonica]
+};
+const secondUser = {
+name: "Wendy",
+gender: "Male",
+age: 23,
+email: "wendy@dingdong.com",
+favoriteColor: [...setColorWendy],
+isHavePet: "No",
+education: educationWendy,
+favoriteRestaurant: [...setRestoWendy]
+};
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
 const users = [];
+users.push(firstUser);
+users.push(secondUser);
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
